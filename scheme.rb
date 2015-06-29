586c4a9c9719f5905e7a052d4f07cdc5222cc835
Sam Davies <seivadmas@gmail.com>
2015-06-28 22:09:14 -0300

    Annotate code and make it clearer

--- scheme.rb
+++ scheme.rb
@@ -6,82 +6,6 @@ class Scheme
   class NullLambdaError < StandardError; end
   class EmptyListError < StandardError; end
 
-  #### PARSING ####
-
-  # Splits a string of characters into an array of tokens, outputting e.g.
-  # => ["(", "begin", "(", "define", "r", "10", ")", "(", "*", "pi", "(", "*", "r", "r", ")", ")", ")"]
-  def tokenize(chars)
-    chars.gsub('(', ' ( ').gsub(')', ' ) ').split.freeze
-  end
-
-  # Reads from a list of tokens and builds a syntax tree
-  # => [:begin, [:define, :r, 10], [:*, :pi, [:*, :r, :r]]]
-  def read_from_tokens_recursive(tokens, tree = [])
-    fail SchemeSyntaxError, "unexpected EOF while reading (did you forget a ')'?)" unless tokens
-    token = tokens.first
-    next_tokens = tokens.drop(1)
-    if token.nil?
-      tree.first
-    elsif token == '('
-      new_sexp, tokens_remaining = read_from_tokens(next_tokens, [])
-      read_from_tokens(tokens_remaining, tree + [new_sexp])
-    elsif token == ')'
-      [tree, next_tokens]
-    else
-      read_from_tokens(next_tokens, tree + [atom(token)])
-    end
-  end
-
-  # An iterative (and probably faster) approach to building a syntax tree
-  # => [:begin, [:define, :r, 10], [:*, :pi, [:*, :r, :r]]]
-  def read_from_tokens_iterative(tokens)
-    branches = []
-    current_branch = branches
-    branch_stack = []
-    mutable_tokens = tokens.dup # avoid causing inexplicable bugs due to mutation of argument
-    result = loop do
-      token = mutable_tokens.shift
-      # puts "current branch: #{current_branch.inspect if current_branch}"
-      # puts "branches: #{branches.inspect}"
-      # puts "next token: #{token}"
-      # puts "---"
-      if token.nil?
-        fail SchemeSyntaxError, "unexpected EOF while reading (did you forget a ')'?)"
-      elsif mutable_tokens.empty?
-        break branches.any? ? branches.first : atom(token)
-      elsif token == '('
-        current_branch = []
-        branches << current_branch
-      elsif token == ')'
-        current_branch = branches.pop
-        parent_branch  = branches.last
-        parent_branch << current_branch
-        current_branch = branches.last
-      else
-        current_branch << atom(token)
-      end
-    end
-    result
-  end
-
-  # Casts a token to Ruby's internal representation based on type inference
-  # Scheme atoms are represented internally as either Integers, Floats or Symbols
-  # depending on the type.
-  # Lists are represented by Ruby arrays.
-  def atom(token)
-    Integer(token)
-  rescue ArgumentError
-    begin
-      Float(token)
-    rescue ArgumentError
-      token.to_sym
-    end
-  end
-
-  def parse(program)
-    read_from_tokens(tokenize(program))
-  end
-
   #### Environment ####
 
   # Implemented functions:
@@ -111,8 +35,85 @@ class Scheme
       :null     => nil
     }.merge(option_env)
     @env.default = :null
+    @reader_type = reader_type
+  end
 
-    self.class.instance_eval("alias_method :read_from_tokens, :read_from_tokens_#{reader_type}")
+  #### PARSING ####
+
+  def parse(program)
+    case @reader_type
+    when :recursive
+      read_from_tokens_recursive(tokenize(program))
+    when :iterative
+      read_from_tokens_iterative(tokenize(program))
+    end
+  end
+
+  # Splits a string of characters into an array of tokens, outputting e.g.
+  # => ["(", "begin", "(", "define", "r", "10", ")", "(", "*", "pi", "(", "*", "r", "r", ")", ")", ")"]
+  def tokenize(chars)
+    chars.gsub('(', ' ( ').gsub(')', ' ) ').split.freeze
+  end
+
+  # Reads from a list of tokens and builds a syntax tree
+  # Works using recursive descent and is purely functional with no mutable state
+  # Elegant but probably slower than the iterative version below and will fail
+  # if we exceed the stack depth in the Ruby interpreter
+  # => [:begin, [:define, :r, 10], [:*, :pi, [:*, :r, :r]]]
+  def read_from_tokens_recursive(tokens, tree = [])
+    fail SchemeSyntaxError, "unexpected EOF while reading (did you forget a ')'?)" unless tokens
+    token = tokens.first
+    next_tokens = tokens.drop(1)
+    if token.nil?
+      tree.first
+    elsif token == '('
+      new_sexp, tokens_remaining = read_from_tokens_recursive(next_tokens, [])
+      read_from_tokens_recursive(tokens_remaining, tree + [new_sexp])
+    elsif token == ')'
+      [tree, next_tokens]
+    else
+      read_from_tokens_recursive(next_tokens, tree + [atom(token)])
+    end
+  end
+
+  # An iterative (uglier but probably faster) approach to building a syntax tree
+  # => [:begin, [:define, :r, 10], [:*, :pi, [:*, :r, :r]]]
+  def read_from_tokens_iterative(tokens)
+    branches = []
+    mutable_tokens = tokens.dup.reverse
+    loop do
+      token = mutable_tokens.pop
+      # puts "current branch: #{current_branch.inspect if current_branch}"
+      # puts "branches: #{branches.inspect}"
+      # puts "next token: #{token}"
+      # puts "---"
+      if token.nil?
+        fail SchemeSyntaxError, "unexpected EOF while reading (did you forget a ')'?)"
+      elsif mutable_tokens.empty?
+        break branches.any? ? branches.first : atom(token)
+      elsif token == '('
+        branches << []
+      elsif token == ')'
+        finished = branches.pop
+        branches.last << finished
+      else
+        branches.last << atom(token)
+      end
+    end
+  end
+
+  # Casts a token to Ruby's internal representation based on type inference
+  # Scheme atoms are represented internally as either Integers, Floats or Symbols
+  # depending on the type.
+  # Lists are represented by Ruby arrays.
+  def atom(token)
+    Integer(token)
+  rescue ArgumentError
+    begin
+      Float(token)
+    rescue ArgumentError
+      token.to_sym
+    end
   end
 
   #### EVAL ####
@@ -120,9 +121,9 @@ class Scheme
   # variable reference      var                         symbol interpreted as variable name, value is variable value. e.g. (:define :r 10), :r => 10
   # constant literal        number                      a number evaluates to itself. e.g. 12 => 12
   # quote                   (quote exp)                 return the literal expression, do not evaluate it (evaluation escape). e.g. (:quote (:+ 1 2)) => (:+ 1 2)
-  # conditional             (if test on_true on_false)  evaluate test, if true evaluate on_true, otherwise on_false. e.g. (:if (:> 10 20) (:+ 1 1) (:+ 3 3)) ⇒ 6
+  # conditional             (if test on_true on_false)  evaluate test, if true evaluate on_true, otherwise on_false. e.g. (:if (:> 10 20) (:+ 1 1) (:+ 3 3)) => 6
   # definition              (define var exp)            define a new variable and give it value as the evaluation of exp. e.g. (:define :r 10)
-  # procedure call          (proc args...)              if proc is anything other than [if, define, quote] then treat it as a procedure. evaluate proc and all the args, then apply proc to list of arg values. e.g. (:sqrt (:* 2 8)) ⇒ 4.0
+  # procedure call          (proc args...)              if proc is anything other than [if, define, quote] then treat it as a procedure. evaluate proc and all the args, then apply proc to list of arg values. e.g. (:sqrt (:* 2 8)) => 4.0
 
   def eval_s(sexp, env = @env)
     if sexp.is_a? Symbol # variable reference
@@ -152,6 +153,8 @@ class Scheme
   def parseval(string)
     eval_s(parse(string))
   end
+
+  ##### REPL #####
 
   def repl
     while buf = Readline.readline("$ ", true)
